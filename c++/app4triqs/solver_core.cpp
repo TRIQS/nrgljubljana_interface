9f8023f8ce6baa5a2a8310831a2e15bdf0371fac
Nils Wentzell <nwentzell@flatironinstitute.org>
2019-10-31 16:02:04 -0400

    Template project for a TRIQS based imaginary time impurity solver

---
+++ c++/app4triqs/solver_core.cpp
@@ -0,0 +1,74 @@
+/*******************************************************************************
+ *
+ * app4triqs: A TRIQS based impurity solver
+ *
+ * Copyright (c) 2019 The Simons foundation
+ *   authors: Nils Wentzell
+ *
+ * app4triqs is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * app4triqs is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * app4triqs. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+#include "./solver_core.hpp"
+#include "./post_process.hpp"
+
+namespace app4triqs {
+
+  solver_core::solver_core(constr_params_t const &p) : constr_params(p) {
+
+    // Initialize the non-interacting Green function
+    G0_iw = block_gf<imfreq>{{p.beta, Fermion, p.n_iw}, p.gf_struct};
+
+    // Initialize the result containers
+    G_tau    = block_gf<imtime>{{p.beta, Fermion, p.n_tau}, p.gf_struct};
+    G_iw     = G0_iw;
+    Sigma_iw = G0_iw;
+  }
+
+  // -------------------------------------------------------------------------------
+
+  void solver_core::solve(solve_params_t const &solve_params) {
+
+    last_solve_params = solve_params;
+
+    if (world.rank() == 0)
+      std::cout << "\n"
+                   "APP4TRIQS Solver\n";
+
+    // Assert hermiticity of the given Weiss field
+    if (!is_gf_hermitian(G0_iw)) TRIQS_RUNTIME_ERROR << "Please make sure that G0_iw fullfills the hermiticity relation G_ij[iw] = G_ji[-iw]*";
+
+    // Merge constr_params and solve_params
+    params_t params(constr_params, solve_params);
+
+    // Reset the results
+    container_set::operator=(container_set{});
+
+    // TODO Solve the impurity model
+
+    // Post Processing
+    if (params.post_process) { post_process(params); }
+  }
+
+  // -------------------------------------------------------------------------------
+
+  void solver_core::post_process(params_t const &p) {
+
+    if (world.rank() == 0)
+      std::cout << "\n"
+                   "Post-processing ... \n";
+
+    // TODO
+  }
+
+} // namespace app4triqs
---
+++ c++/app4triqs/solver_core.hpp
@@ -0,0 +1,108 @@
+/*******************************************************************************
+ *
+ * app4triqs: A TRIQS based impurity solver
+ *
+ * Copyright (c) 2019 The Simons foundation
+ *   authors: Nils Wentzell
+ *
+ * app4triqs is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * app4triqs is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * app4triqs. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+#pragma once
+#include "./container_set.hpp"
+#include "./params.hpp"
+#include "./types.hpp"
+
+namespace app4triqs {
+
+  /// The Solver class
+  class solver_core : public container_set {
+
+    private:
+    // Mpi Communicator
+    mpi::communicator world;
+
+    // Return reference to container_set
+    container_set &result_set() { return static_cast<container_set &>(*this); }
+    container_set const &result_set() const { return static_cast<container_set const &>(*this); }
+
+    // Function to perform the post-processing steps
+    void post_process(params_t const &p);
+
+    public:
+    /**
+     * Construct a APP4TRIQS solver
+     *
+     * @param construct_parameters Set of parameters specific to the APP4TRIQS solver
+     */
+    CPP2PY_ARG_AS_DICT
+    solver_core(constr_params_t const &constr_params_);
+
+    // Delete assignement operator because of const members
+    solver_core(solver_core const &p) = default;
+    solver_core(solver_core &&p)      = default;
+    ~solver_core()                    = default;
+    solver_core &operator=(solver_core const &p) = delete;
+    solver_core &operator=(solver_core &&p) = default;
+
+    /**
+     * Solve method that performs APP4TRIQS calculation
+     *
+     * @param solve_params_t Set of parameters specific to the APP4TRIQS run
+     */
+    CPP2PY_ARG_AS_DICT
+    void solve(solve_params_t const &solve_params);
+
+    // Struct containing the parameters relevant for the solver construction
+    constr_params_t constr_params;
+
+    // Struct containing the parameters relevant for the solve process
+    std::optional<solve_params_t> last_solve_params;
+
+    /// Noninteracting Green Function in Matsubara frequencies
+    g_iw_t G0_iw;
+
+    // Allow the user to retrigger post-processing with the last set of parameters
+    void post_process() {
+      if (not last_solve_params) TRIQS_RUNTIME_ERROR << "You need to run the solver once before you post-process";
+      post_process({constr_params, last_solve_params.value()});
+    }
+
+    static std::string hdf5_scheme() { return "APP4TRIQS_SolverCore"; }
+
+    // Function that writes a solver object to hdf5 file
+    friend void h5_write(triqs::h5::group h5group, std::string subgroup_name, solver_core const &s) {
+      auto grp = h5group.create_group(subgroup_name);
+      h5_write_attribute(grp, "TRIQS_HDF5_data_scheme", solver_core::hdf5_scheme());
+      h5_write_attribute(grp, "TRIQS_GIT_HASH", std::string(AS_STRING(TRIQS_GIT_HASH)));
+      h5_write_attribute(grp, "APP4TRIQS_GIT_HASH", std::string(AS_STRING(APP4TRIQS_GIT_HASH)));
+      h5_write(grp, "", s.result_set());
+      h5_write(grp, "constr_params", s.constr_params);
+      h5_write(grp, "last_solve_params", s.last_solve_params);
+      h5_write(grp, "G0_iw", s.G0_iw);
+    }
+
+    // Function that constructs a solver object from an hdf5 file
+    CPP2PY_IGNORE
+    static solver_core h5_read_construct(triqs::h5::group h5group, std::string subgroup_name) {
+      auto grp           = h5group.open_group(subgroup_name);
+      auto constr_params = h5_read<constr_params_t>(grp, "constr_params");
+      auto s             = solver_core{constr_params};
+      h5_read(grp, "", s.result_set());
+      h5_read(grp, "last_solve_params", s.last_solve_params);
+      h5_read(grp, "G0_iw", s.G0_iw);
+      return s;
+    }
+  };
+} // namespace app4triqs
