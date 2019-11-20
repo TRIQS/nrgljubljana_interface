96ec4a3a66b2a432bad4ed44b67030c604c68729
Rok Zitko <rok.zitko@ijs.si>
2019-11-20 12:39:00 +0100

    templated fixes

---
+++ templates/2orb-UJ/2orb-UJ.m
@@ -0,0 +1,26 @@
+def2ch[nrimp=2];
+
+Heps1 = eps1 number[d[]];
+Hint1 = U1 hubbard[d[]];
+Hhyb1 = gammaPol1 hop[f[0], d[]];
+H1 = Heps1 + Hint1;
+
+Heps2 = eps2 number[a[]];
+Hint2 = U2 hubbard[a[]];
+Hhyb2 = gammaPol2 hop[f[1], a[]];
+H2 = Heps2 + Hint2;
+
+H12 = U12 nc[number[d[]], number[a[]]]+ J12 spinspin[d[], a[]];
+
+Heps = Heps1 + Heps2;
+Hint = Hint1 + Hint2 + H12;
+Hhyb = Hhyb1 + Hhyb2;
+H = H0 + Heps + Hint + Hhyb;
+
+params = Join[params, {
+  gammaPol1 -> Sqrt[(1/Pi) thetaCh[1] (extraGamma1) gammaA],
+  gammaPol2 -> Sqrt[(1/Pi) thetaCh[2] (extraGamma2) gammaA]
+}];
+
+selfopd = ( Chop @ Expand @ komutator[Hint /. params, d[#1, #2]] )&;
+selfopa = ( Chop @ Expand @ komutator[Hint /. params, a[#1, #2]] )&;
---
+++ test/python/3_2orb-UJ_QS.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python2
+
+import unittest
+
+from nrgljubljana_interface import Solver, FlatNew, SemiCircularNew
+
+from pytriqs.gf import *
+from pytriqs.archive import *
+from pytriqs.operators import *
+from pytriqs.utility.h5diff import h5diff
+
+
+class test_SIAM(unittest.TestCase):
+
+    # Construct Parameters
+    cp = {}
+    cp["model"] = "2orb-UJ"
+    cp["symtype"] = "QS"
+    cp["mesh_max"] = 10.0
+    cp["mesh_min"] = 1e-6
+    cp["mesh_ratio"] = 1.03
+
+    # Set up the Solver
+    S = Solver(**cp)
+
+    # Solve Parameters
+    sp = {}
+    sp["Lambda"] = 4.0
+    sp["Nz"] = 2
+    sp["Tmin"] = 1e-7
+    sp["keep"] = 300
+    sp["keepenergy"] = 8.0
+
+    # Model Parameters
+    mp = {}
+    mp["U1"] = 1.0
+    mp["U2"] = 1.0
+    mp["eps1"] = -0.4
+    mp["eps2"] = -0.4
+    mp["U12"] = 1.0
+    mp["J12"] = 0
+    sp["model_parameters"] = mp
+
+    # Low-level NRG Parameters
+    np = {}
+    np["bandrescale"] = 1 # band support is [-1:1]
+    S.set_nrg_params(**np)
+
+    # # Initialize hybridization function
+    S.Delta_w['imp'][0,0] << 0.1 * SemiCircularNew(1.0)
+    S.Delta_w['imp'][1,1] << 0.1 * SemiCircularNew(1.0)
+    # Out-of-diagonal Delta is zero
+
+    # Solve the impurity model
+    S.solve(**sp)
+
+    # # Store the Result
+    with HDFArchive("SIAM.out.h5", 'w') as arch:
+        arch["S"] = S
+
+    # # # Compare against reference result
+    # # h5diff("SIAM.out.h5", "SIAM.ref.h5")
+
+if __name__ == '__main__':
+    unittest.main()
