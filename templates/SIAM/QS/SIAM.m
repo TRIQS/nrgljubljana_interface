7b48efe18ae193aa5915e28769f0cb0ed501c48b
Rok Zitko <rok.zitko@ijs.si>
2019-11-21 16:10:38 +0100

    self_d as file

--- templates/SIAM/QS/SIAM.m
+++ templates/SIAM/QS/SIAM.m
@@ -4,4 +4,4 @@ Hint = U1 hubbard[d[]];
 Himp = Heps + Hint;
 Hhyb = gammaPolCh[1] hop[f[0], d[]];
 H = H0 + Himp + Hhyb;
-selfopd = ( Chop @ Expand @ komutator[Hint /. params, d[#1, #2]] )&;
+selfopd = ( Chop @ Expand @ komutator[Hint, d[#1, #2]] )&;
---
+++ test/python/4_mesh_max=10.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python2
+
+import unittest
+
+from nrgljubljana_interface import Solver, FlatNew, SemiCircularNew
+
+from pytriqs.gf import *
+from pytriqs.archive import *
+from pytriqs.operators import *
+from pytriqs.utility.h5diff import h5diff
+
+
+class test_SIAM(unittest.TestCase):
+
+    # Construct Parameters
+    cp = {}
+    cp["model"] = "SIAM"
+    cp["symtype"] = "QS"
+    cp["mesh_max"] = 10.0
+    cp["mesh_min"] = 1e-6
+    cp["mesh_ratio"] = 1.03
+
+    # Set up the Solver
+    S = Solver(**cp)
+
+    # Solve Parameters
+    sp = {}
+    sp["Lambda"] = 2.0
+    sp["Nz"] = 2
+    sp["Tmin"] = 1e-7
+    sp["keep"] = 100
+    sp["keepenergy"] = 8.0
+
+    # Model Parameters
+    mp = {}
+    mp["U1"] = 0.2
+    mp["eps1"] = -0.08
+    sp["model_parameters"] = mp
+
+    # Low-level NRG Parameters
+    #np = {}
+    #np["bandrescale"] = 10
+    #S.set_nrg_params(**np)
+
+    # # Initialize hybridization function
+    # for w in S.Delta_w.mesh: S.Delta_w[w] = 1 / w
+    S.Delta_w['imp'] << 0.01 * SemiCircularNew(1.0)
+
+    # Solve the impurity model
+    S.solve(**sp)
+
+    # # Store the Result
+    with HDFArchive("SIAM.out.h5", 'w') as arch:
+        arch["S"] = S
+
+    # # # Compare against reference result
+    # # h5diff("SIAM.out.h5", "SIAM.ref.h5")
+
+if __name__ == '__main__':
+    unittest.main()
