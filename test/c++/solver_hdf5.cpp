15519599ecce6afcc3e02b7dd79b3cfe5c212b9d
Rok Zitko <rok.zitko@ijs.si>
2020-02-04 16:02:37 +0100

    temporarily commented out due to lack of Mac support

--- test/c++/solver_hdf5.cpp
+++ test/c++/solver_hdf5.cpp
@@ -57,6 +57,6 @@ int main(int argc, char **argv) {
 
   // Rerun the Solver and Compare
   auto S_old = solver_core::h5_read_construct(file("Solver.h5", 'r'), "S");
-  S_old.solve(S_old.last_solve_params.value());
-//  assert_block_gfs_are_close(S_old.G_w, S.G_w, 1e-16);
+  // S_old.solve(S_old.last_solve_params.value()); // TO DO
+//  assert_block_gfs_are_close(S_old.G_w, S.G_w, 1e-16); // TO DO
 }
