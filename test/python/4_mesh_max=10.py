b3f82c6bf46cddc0b919b12a15b2921c76edb2e9
Thomas Hahn <thomas.hahn01@gmail.com>
2025-10-01 14:29:12 -0400

    Fix unrecognized python tests

--- test/python/4_mesh_max=10.py
+++ test/python/4_mesh_max=10.py
@@ -1,63 +1,64 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import unittest
 
 from nrgljubljana_interface import Solver, SemiCircular
 
-from h5 import *
+from h5 import HDFArchive
 from triqs.utility.h5diff import h5diff
 
 
 class test_SIAM(unittest.TestCase):
 
-    # Construct Parameters
-    cp = {}
-    cp["model"] = "SIAM"
-    cp["symtype"] = "QS"
-    cp["mesh_max"] = 10.0
-    cp["mesh_min"] = 1e-3
-    cp["mesh_ratio"] = 1.1
+    def test_mesh_max_10_solver(self):
+        # Construct Parameters
+        cp = {}
+        cp["model"] = "SIAM"
+        cp["symtype"] = "QS"
+        cp["mesh_max"] = 10.0
+        cp["mesh_min"] = 1e-3
+        cp["mesh_ratio"] = 1.1
 
-    # Set up the Solver
-    S = Solver(**cp)
+        # Set up the Solver
+        S = Solver(**cp)
 
-    # Solve Parameters
-    sp = {}
-    sp["T"] = 1e-3
-    sp["Lambda"] = 4.0
-    sp["Nz"] = 2
-    sp["Tmin"] = 1e-4
-    sp["keep"] = 50
-    sp["keepenergy"] = 6.0
+        # Solve Parameters
+        sp = {}
+        sp["T"] = 1e-3
+        sp["Lambda"] = 4.0
+        sp["Nz"] = 2
+        sp["Tmin"] = 1e-4
+        sp["keep"] = 50
+        sp["keepenergy"] = 6.0
 
-    # Model Parameters
-    mp = {}
-    mp["U1"] = 0.5
-    mp["eps1"] = -0.24
-    sp["model_parameters"] = mp
+        # Model Parameters
+        mp = {}
+        mp["U1"] = 0.5
+        mp["eps1"] = -0.24
+        sp["model_parameters"] = mp
 
-    # Low-level NRG Parameters
-    np = {}
-    np["bins"] = 50
-    S.set_nrg_params(**np)
+        # Low-level NRG Parameters
+        np = {}
+        np["bins"] = 50
+        S.set_nrg_params(**np)
 
-    # # Initialize hybridization function
-    S.Delta_w['imp'] << 0.1 * SemiCircular(1.0)
+        # # Initialize hybridization function
+        S.Delta_w['imp'] << 0.1 * SemiCircular(1.0)
 
-    # Solve the impurity model
-    S.solve(**sp)
+        # Solve the impurity model
+        S.solve(**sp)
 
-    # # Store the Result
-    with HDFArchive("4_mesh_max=10.out.h5", 'w') as arch:
-        arch["A_w"] = S.A_w
-        arch["G_w"] = S.G_w
-        arch["F_l_w"] = S.F_l_w
-        arch["F_r_w"] = S.F_r_w
-        arch["I_w"] = S.I_w
-        arch["Sigma_w"] = S.Sigma_w
+        # # Store the Result
+        with HDFArchive("4_mesh_max=10.out.h5", 'w') as arch:
+            arch["A_w"] = S.A_w
+            arch["G_w"] = S.G_w
+            arch["F_l_w"] = S.F_l_w
+            arch["F_r_w"] = S.F_r_w
+            arch["I_w"] = S.I_w
+            arch["Sigma_w"] = S.Sigma_w
 
-    # Compare against reference result
-    h5diff("4_mesh_max=10.out.h5", "4_mesh_max=10.ref.h5")
+        # Compare against reference result
+        h5diff("4_mesh_max=10.out.h5", "4_mesh_max=10.ref.h5")
 
 if __name__ == '__main__':
     unittest.main()
--- test/python/6_solver_h5io.py
+++ test/python/6_solver_h5io.py
@@ -1,59 +1,60 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import unittest
 
 from nrgljubljana_interface import Solver, SemiCircular, MeshReFreqPts
 
-from h5 import *
+from h5 import HDFArchive
 from triqs.utility.h5diff import h5diff
 from triqs.utility import mpi
 from triqs.utility.comparison_tests import *
 
 class test_SIAM(unittest.TestCase):
 
-    # Construct Parameters
-    cp = {}
-    cp["model"] = "SIAM"
-    cp["symtype"] = "QS"
-    cp["mesh_max"] = 1.0
-    cp["mesh_min"] = 1e-3
-    cp["mesh_ratio"] = 1.1
+    def test_solver_h5io(self):
+        # Construct Parameters
+        cp = {}
+        cp["model"] = "SIAM"
+        cp["symtype"] = "QS"
+        cp["mesh_max"] = 1.0
+        cp["mesh_min"] = 1e-3
+        cp["mesh_ratio"] = 1.1
 
-    # Set up the Solver
-    S = Solver(**cp)
+        # Set up the Solver
+        S = Solver(**cp)
 
-    # Solve Parameters
-    sp = {}
-    sp["T"] = 1e-3
-    sp["Lambda"] = 4.0
-    sp["Nz"] = 2
-    sp["Tmin"] = 1e-4
-    sp["keep"] = 50
-    sp["keepenergy"] = 6.0
+        # Solve Parameters
+        sp = {}
+        sp["T"] = 1e-3
+        sp["Lambda"] = 4.0
+        sp["Nz"] = 2
+        sp["Tmin"] = 1e-4
+        sp["keep"] = 50
+        sp["keepenergy"] = 6.0
 
-    # Model Parameters
-    mp = {}
-    mp["U1"] = 0.5
-    mp["eps1"] = -0.24
-    sp["model_parameters"] = mp
+        # Model Parameters
+        mp = {}
+        mp["U1"] = 0.5
+        mp["eps1"] = -0.24
+        sp["model_parameters"] = mp
 
-    # Low-level NRG Parameters
-    np = {}
-    np["bins"] = 50
-    S.set_nrg_params(**np)
+        # Low-level NRG Parameters
+        np = {}
+        np["bins"] = 50
+        S.set_nrg_params(**np)
 
-    # # Initialize hybridization function
-    S.Delta_w['imp'] << 0.1 * SemiCircular(1.0)
+        # # Initialize hybridization function
+        S.Delta_w['imp'] << 0.1 * SemiCircular(1.0)
 
-    # Solve the impurity model
-    S.solve(**sp)
+        # Solve the impurity model
+        S.solve(**sp)
 
-    if mpi.is_master_node(): HDFArchive('Solver.h5', 'w')['S'] = S
+        if mpi.is_master_node(): HDFArchive('Solver.h5', 'w')['S'] = S
 
-    # Rerun the Solver and Compare
-    S_old = HDFArchive('Solver.h5', 'r')['S']
-    S_old.solve(**S_old.last_solve_params)
-    assert_block_gfs_are_close(S_old.G_w, S.G_w, 1e-12)
+        # Rerun the Solver and Compare
+        S_old = HDFArchive('Solver.h5', 'r')['S']
+        S_old.solve(**S_old.last_solve_params)
+        assert_block_gfs_are_close(S_old.G_w, S.G_w, 1e-12)
 
 if __name__ == '__main__':
     unittest.main()
